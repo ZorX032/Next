34576256ce71ac9daed7e672bb17673c15658ef4
ZorX032 <shuriknich032@gmail.com>
2025-02-06 19:13:56 +0200

    next metadata/links

--- src/app/(public)/users/[id]/page.tsx
+++ src/app/(public)/users/[id]/page.tsx
@@ -1,6 +1,17 @@
 import React, {FC} from 'react';
+import {Metadata} from "next";
 type Props = {
     params: { id: string;}
+}
+
+
+export const generateMetadata = async ({params}: Props):Promise<Metadata>=>{
+    const {id} = await params;
+
+    return {
+        title: 'User page title' + id,
+
+    }
 }
 const UserPage: FC<Props> = async ({params}) => {
 

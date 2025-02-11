b0fb26dd4783658cdee5a62f459cc91403e28124
ZorX032 <shuriknich032@gmail.com>
2025-02-11 16:35:26 +0200

    next hw_2 end

--- src/components/menu/MenuComponent.tsx
+++ src/components/menu/MenuComponent.tsx
@@ -5,10 +5,10 @@ const Menu = () => {
     return (
         <ul className="flex space-x-4 bg-gray-800 p-4 text-white">
             <li>
-                <Link to='/cars' className="hover:text-blue-400">Cars</Link>
+                <Link href='/cars' className="hover:text-blue-400">Cars</Link>
             </li>
             <li>
-                <Link to='/cars/create' className="hover:text-blue-400">Create</Link>
+                <Link href='/create' className="hover:text-blue-400">Create</Link>
             </li>
         </ul>
     );
